75452e915f3e8fc66e98e7ea41d652175cc592c4
pyama2000 <takahiko-yamashita@retty.me>
2023-07-29 18:38:20 +0900

    feat: graceful shutdown server

--- service/tenant-service/server/src/main.rs
+++ service/tenant-service/server/src/main.rs
@@ -4,6 +4,8 @@ mod config;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt::init();
+
     let config = config::Config::from_env();
 
     let reflection_service = tonic_reflection::server::Builder::configure()
@@ -11,9 +13,36 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build()?;
 
     let addr = format!("0.0.0.0:{}", &config.port).parse()?;
+    tracing::info!("TenentService listening on: {}", &addr);
     tonic::transport::Server::builder()
         .add_service(reflection_service)
-        .serve(addr)
+        .serve_with_shutdown(addr, shutdown_signal())
         .await?;
     Ok(())
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .unwrap_or_else(|e| panic!("failed to install Ctrl+C handler: {}", e))
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .unwrap_or_else(|e| panic!("failed to install singal handler: {}", e))
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {tracing::info!("receive ctrl_c signal")},
+        _ = terminate => {tracing::info!("receive terminate")},
+    }
+
+    tracing::info!("signal received, starting graceful shutdown");
 }
